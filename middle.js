9ccf1e94389d9c9ea0c52a589b62eb5218cee711
SFxImpulse <braverman.david.business@gmail.com>
2022-06-29 16:42:41 +0000

    Added a function that takes the middle values from an array

---
+++ middle.js
@@ -0,0 +1,14 @@
+const middle = function(array) {
+  output = [];
+  const middleIndex = array[Math.floor(array.length / 2)];
+  for (let i = 0; i < array.length; i++) {
+    if (array.length <= 2) {
+      return output;
+    } else if (array.length % 2 === 0 + 1 && array[i] === middleIndex) {
+      output.push(array[i]);
+    } else if (array.length % 2 === 0 && array[i] === middleIndex) {
+      output.push(array[i - 1], array[i]);
+    }
+  }
+  return output;
+}
