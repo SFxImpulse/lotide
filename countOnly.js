e43dba37d14e9932fae0dfc065f5dcea56d10007
SFxImpulse <braverman.david.business@gmail.com>
2022-06-29 18:02:00 +0000

    fixed the file lol

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else if (actual !== expected) {
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   
@@ -20,28 +12,3 @@ const countOnly = function(allItems, itemsToCount) {
   }
   return results;
 }
-
-// Define function countOnly.
-// countOnly will be given an array and an object.
-  //
-// Returns an object containing counts of-
-// everything that the input object listed.
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-console.log(assertEqual(result1["Jason"], 1));
-console.log(assertEqual(result1["Karima"], undefined));
-console.log(assertEqual(result1["Fang"], 2));
-console.log(assertEqual(result1["Agouhanna"], undefined));
