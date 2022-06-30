c6854b2636108508b1ad872d5506a009bf889f5a
SFxImpulse <braverman.david.business@gmail.com>
2022-06-30 01:20:19 +0000

    added an array that asserts objects are equal

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -36,3 +36,11 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    return `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
+  } else if (actual !== expected) {
+    return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
+  }
+};
