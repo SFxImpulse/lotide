da30d88005e24db78008f3c0f04bd3366ff75cd5
SFxImpulse <braverman.david.business@gmail.com>
2022-06-30 19:34:25 +0000

    added a function that returns array values until reaching a specific value and stopping

---
+++ takeUntil.js
@@ -0,0 +1,12 @@
+const takeUntil = (array, callback) => {
+
+  const output = [];
+
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    }
+    output.push(item)
+  }
+  return output;
+};
